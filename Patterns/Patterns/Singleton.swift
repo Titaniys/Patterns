25e491766e510ee8309d94ba875efa7c49521008
Вадим Чистяков <chistyakov.v.e@sberbank.ru>
2018-06-10 17:24:10 +0300

    Singleton

---
+++ Patterns/Patterns/Singleton.swift
@@ -0,0 +1,13 @@
+//
+//  Singleton.swift
+//  Patterns
+//
+//  Created by Вадим Чистяков on 10.06.2018.
+//  Copyright © 2018 Вадим Чистяков. All rights reserved.
+//
+
+import UIKit
+
+class Singleton: NSObject {
+
+}
